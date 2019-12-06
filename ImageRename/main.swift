eaad85cc0b07dc8dc209431c3bccd6a2e80ae0fc
Ben Schultz <bens4lsu@gmail.com>
2019-12-06 09:38:46 -0600

    Initial Commit

---
+++ ImageRename/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ImageRename
+//
+//  Created by Ben Schultz on 12/6/19.
+//  Copyright © 2019 com.concordbusinessservicesllc. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
